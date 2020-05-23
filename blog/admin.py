b1b2d1465c9a707e9943e2a4ef87c2f5266a9132
mstach0wicz <mikolaj.stachowicz@yahoo.com>
2020-05-23 22:02:04 +0200

    First Django app, initial commit

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from blog.models import Post
+
+# Register your models here.
+# rejestrujemy nasz model w panelu admina
+admin.site.register(Post)
---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Tworzymy obiekt dla naszych postów na blogu 
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200) # Definiujemy ilość znaków w tytule 
+    text = models.TextField() # Tu będzie nasz wpis na bloga
+    created_date = models.DateTimeField(default=timezone.now) # Data stworzenia posta
+    published_date = models.DateTimeField(blank=True, null=True) # Data opublikowania posta
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+    # metoda zwraca sama siebie, czyli wywołując __string__() otrzymamy tytuł
+    def __str__(self):
+        return self.title
