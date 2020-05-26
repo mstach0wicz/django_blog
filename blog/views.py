810656221e0276d33a044966932491db24626018
mstach0wicz <mikolaj.stachowicz@yahoo.com>
2020-05-26 18:16:50 +0200

    Zmodyfikowałem szablon, żeby wyświetlał posty z bazy danych.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post # Importujemy nasz model Post
 
-# Create your views here.
-# tworzymy funkcję która bierze żądanie i zwraca wartość z funkcji render
+# Tworzymy funkcję która bierze żądanie i zwraca wartość z funkcji render
 # render złoży nasz template blog/post_list.html
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # Tworzymy zmienną 'posts' dla naszego QuerySetu
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts}) # Przekazujemy dane do naszego szablonu 
