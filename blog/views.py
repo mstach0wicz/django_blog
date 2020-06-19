81865bed06f8dadb9e9304cb83db5374298780e7
mstach0wicz <mikolaj.stachowicz@yahoo.com>
2020-06-19 10:17:40 +0200

    Zmiana w widoku views.py na sortowanie postów wg najnowszej daty

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .forms import PostForm # Importujemy model PostForm
 # render złoży nasz template blog/post_list.html
 def post_list(request):
     # Tworzymy zmienną 'posts' dla naszego QuerySetu i ustawiamy filtrowanie wg daty publikacji postu
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts}) # Przekazujemy dane do naszego szablonu 
 
 def post_detail(request, pk):
